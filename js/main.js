2b0b3b4bd6f5a9cf13d3c0b5361f090931d8640e
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-06 17:47:27 +0200

    Completata prima parte esercizio

--- js/main.js
+++ js/main.js
@@ -21,11 +21,35 @@ console.log(bombe);
 
 //In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
 
+var numUser;
+var listaNumUser = [];
+var gioco = true;
+var punteggio = 0;
 
 
 
+while (gioco) {
+    numUser = parseInt(prompt('Inserisci un numero da 1 a 100'));
+    if (bombe.includes(numUser)) {
+        console.log('Hai perso');
+        console.log('Il tuo punteggio è' + punteggio);
+        
+        gioco = false;
+}
+else if (listaNumUser.includes(numUser)) {
+    console.log('inserisci un numero che non hai mai inserito');
+    
+}
+else {
+    listaNumUser.push(numUser);
+    console.log('vai avanti');
+    punteggio++;
+    
+}
+}
 
-//Se il numero è presente nella lista dei numeri generati (numeri vietati), la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
+//Se il numero  è presente nella lista dei numeri generati BOMBE (numeri vietati), la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 
 
 
